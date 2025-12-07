2b29526df83ac3176c17fca3df45696d9b41239f
Jason Aldrich <admin@isolex.net>
2025-12-07 12:22:40 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -4,6 +4,7 @@ import Whitepaper from './pages/Whitepaper';
 import Oracle from './pages/Oracle';
 import Profile from './pages/Profile';
 import Sanctum from './pages/Sanctum';
+import sanctum from './pages/sanctum';
 import __Layout from './Layout.jsx';
 
 
@@ -14,6 +15,7 @@ export const PAGES = {
     "Oracle": Oracle,
     "Profile": Profile,
     "Sanctum": Sanctum,
+    "sanctum": sanctum,
 }
 
 export const pagesConfig = {
