27aa79b70ed517e4874bac9c3bee89fd418e13c4
Jason Aldrich <admin@isolex.net>
2025-12-07 17:14:42 +0000

    File changes

--- src/components/shared/NFTGate.jsx
+++ src/components/shared/NFTGate.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { createPageUrl } from '../utils';
 import { useQuery } from '@tanstack/react-query';
 import { base44 } from '@/api/base44Client';
-import { useWeb3 } from '../../layout';
+import { useWeb3 } from '../../Layout';
 
 export default function NFTGate({ children, fallback }) {
   const { account } = useWeb3();
