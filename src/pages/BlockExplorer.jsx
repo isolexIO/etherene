020d2df40d556242f2a4da60b3b87e2b9b62af27
Jason Aldrich <admin@isolex.net>
2025-12-09 02:25:02 +0000

    File changes

--- src/pages/BlockExplorer.jsx
+++ src/pages/BlockExplorer.jsx
@@ -155,13 +155,7 @@ export default function BlockExplorer() {
     };
   }, []);
 
-  const addToWatchlist = useCallback((address) => {
-    setWatchedAddresses(prev => [...prev, address]);
-  }, []);
 
-  const removeFromWatchlist = useCallback((address) => {
-    setWatchedAddresses(prev => prev.filter(a => a !== address));
-  }, []);
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
